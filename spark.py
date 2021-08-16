ae3ceb512d589e25c4f12367bd44ed3d8d42261d
Ivan Payne <76186695+KngPayne@users.noreply.github.com>
2021-08-16 04:03:25 -0500

    Add files via upload
    Two main files for the twitter scrapper project

---
+++ spark.py
@@ -0,0 +1,101 @@
+from pyspark import SparkConf, SparkContext
+from pyspark.streaming import StreamingContext
+from textblob import TextBlob
+from elasticsearch import Elasticsearch
+from ftfy import fix_text
+from googletrans import Translator
+from geopy.geocoders import Nominatim
+
+TCP_IP = 'localhost'
+TCP_PORT = 9001
+
+
+# initialzie geolocator and start translator
+# translator = Translator()
+#
+#
+# clean tweet locations into english
+# def clean_text(text, engl=True):
+#     if engl:
+#         trans_word = text
+#     else:
+#         trans_word = translator.translate(text).text
+#
+#     token_text = []
+#     text = trans_word
+#     text = [fix_text(str(i)) for i in text]
+#
+#    return text
+
+def processTweet(tweet):
+    # Here, you should implement:
+    # (i) Sentiment analysis,
+    # (ii) Get data corresponding to place where the tweet was generate (using geopy or googlemaps)
+    # (iii) Index the data using Elastic Search
+
+    # Elastic search initialization
+    cloud_id = '4371-Project:ZWFzdHVzMi5henVyZS5lbGFzdGljLWNsb3VkLmNvbTo5MjQzJDlhZDk2NTcxZWFiMjQ0OTU5NDVmMzJlOTIyYjRmYjBlJDkxMmYxMTZmMDg4MDQ3NmE5MDcwMDVkYTkzZDAwNzAx'
+    es = Elasticsearch(cloud_id=cloud_id, http_auth=('elastic', 'vrjToaJjSHLZZU6yTI519QpC'))
+
+    tweetData = tweet.split("::")
+
+    if len(tweetData) > 1:
+
+        text = tweetData[1]
+        rawLocation = tweetData[0]
+
+        # (i) Apply Sentiment analysis in "text"
+        if float(TextBlob(text).sentiment.polarity) > 0.3:
+            sentiment = "Positive"
+        elif float(TextBlob(text).sentiment.polarity) < -0.3:
+            sentiment = "Negative"
+        else:
+            sentiment = "Neutral"
+
+        # (ii) Get geolocation (state, country, lat, lon, etc...) from rawLocation
+        try:
+            geolocator = Nominatim(user_agent="bts")
+            location = geolocator.geocode(tweetData[0], exactly_one=True, addressdetails=True)
+            lat = location.raw['lat']
+            lon = location.raw['lon']
+            state = location.raw['address']['state']
+            country = location.raw['address']['country']
+        except:
+            lat = lon = state = country = None
+
+        print("\n\n=========================\ntweet: ", tweet)
+        print("Raw location from tweet status: ", rawLocation)
+        # print("lat: ", lat)
+        # print("lon: ", lon)
+        # print("state: ", state)
+        # print("country: ", country)
+        # print("Text: ", text)
+        # print("Sentiment: ", sentiment)
+
+        # (iii) Post the index on ElasticSearch or log your data in some other way (you are always free!!)
+        if lat != None and lon != None and sentiment != None:
+            esDoc = {"location": {"lat": lat, "lon": lon}, "state": state, "country": country, "sentiment": sentiment}
+            es.index(index='tweet-geo', doc_type='default', body=esDoc)
+
+
+# Pyspark
+# create spark configuration
+conf = SparkConf()
+conf.setAppName('TwitterApp')
+conf.setMaster('local[2]')
+
+# create spark context with the above configuration
+sc = SparkContext(conf=conf)
+
+# create the Streaming Context from spark context with interval size 4 seconds
+ssc = StreamingContext(sc, 4)
+ssc.checkpoint("checkpoint_TwitterApp")
+
+# read data from port 900
+dataStream = ssc.socketTextStream(TCP_IP, TCP_PORT)
+
+# process eachRDD apply processTweet
+dataStream.foreachRDD(lambda rdd: rdd.foreach(processTweet))
+
+ssc.start()
+ssc.awaitTermination()
---
+++ stream.py
@@ -0,0 +1,101 @@
+import tweepy
+import socket
+import re
+
+# Enter your Twitter keys here!!!
+ACCESS_TOKEN = "1387165894200135682-k5Rfa7HmEwsUb55JXFPpDbFFfamqAm"
+ACCESS_SECRET = "tKR4O906JYojOvhtWsGf5SaGkMH4j62wrh1tebS9kwhBj"
+CONSUMER_KEY = "t1dTRVrQgJHAQcMjHZ8wrQxs8"
+CONSUMER_SECRET = "Vdw4eLaW6f6Qm38CJLpE5g1xqoR7x5LMpjsFk3QPWFfitF6ypI"
+
+# API authentication
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
+
+# create wrapper
+api = tweepy.API(auth)
+
+hashtag = "#palestine"
+
+filename = "TweetScrapper.csv"
+
+# stream location
+TCP_IP = 'localhost'
+TCP_PORT = 9001
+
+
+def preprocessing(tweet):
+    # Add here your code to preprocess the tweets and
+    # remove Emoji patterns, emoticons, symbols & pictographs, transport & map symbols, flags (iOS), etc
+    # remove additional spacing as well
+    emoji_pattern = re.compile("["
+                               u"\U0001F600-\U0001F64F"  # emoticons
+                               u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+                               u"\U0001F680-\U0001F6FF"  # transport & map symbols
+                               u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                               u"\U00002500-\U00002BEF"  # chinese char
+                               u"\U00002702-\U000027B0"
+                               u"\U000024C2-\U0001F251"
+                               u"\U0001f926-\U0001f937"
+                               u"\U00010000-\U0010ffff]+", flags=re.UNICODE)
+    tweet = re.sub(r'[^\x00-\x7F]+', ' ', tweet)
+    text = emoji_pattern.sub(r'', tweet)
+    text = text.replace("\n", " ")
+    return text
+
+
+def getTweet(status):
+    # You can explore fields/data other than location and the tweet itself.
+    # Check what else you could explore in terms of data inside Status object
+
+    tweet = ""
+    location = ""
+
+    location = status.user.location
+
+    if hasattr(status, "retweeted_status"):  # Check if Retweet
+        try:
+            tweet = status.retweeted_status.extended_tweet["full_text"]
+        except AttributeError:
+            tweet = status.retweeted_status.text
+    else:
+        try:
+            tweet = status.extended_tweet["full_text"]
+        except AttributeError:
+            tweet = status.text
+
+    return location, preprocessing(tweet)
+
+
+# create sockets
+# comment out if current distro is not set up to be streamed
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((TCP_IP, TCP_PORT))
+s.listen(1)
+conn, addr = s.accept()
+
+
+# error handling
+class MyStreamListener(tweepy.StreamListener):
+
+    def on_status(self, status):
+        location, tweet = getTweet(status)
+
+        if (location != None and tweet != None):
+            tweetLocation = location + "::" + tweet + "\n"
+            print(location, status.text)
+            # comment out if current distro is not set up to be streamed
+            conn.send(tweetLocation.encode('utf-8'))
+
+        return True
+
+    def on_error(self, status_code):
+        if status_code == 420:
+            return False
+        else:
+            print(status_code)
+
+
+# stream listener creation
+myStream = tweepy.Stream(auth=auth, listener=MyStreamListener())
+myStream.filter(track=[hashtag], languages=["en"])
